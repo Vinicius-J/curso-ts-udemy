2165091596588191bd5c1f40f215fc45dc77f2a7
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2026-01-14 09:50:44 -0300

    A0057-decorators-property

---
+++ src/A0057-decorators-property/A0057-decorators-property.ts
@@ -0,0 +1,49 @@
+function decorador(classPrototype: any, nome: string | symbol): any {
+  let valorPropriedade: any;
+
+  return {
+    get: () => valorPropriedade,
+    set: (valor: any) => {
+      if (typeof valor === 'string') {
+        valorPropriedade = valor.split('').reverse().join();
+        return;
+      }
+      valorPropriedade = valor;
+    },
+  };
+}
+
+export class UmaPessoa {
+  @decorador
+  nome: string;
+  @decorador
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
+
+const pessoa = new UmaPessoa('Vinícius', 'Joaquim', 23);
+// pessoa.metodo = () => 'EIIII';
+const metodo = pessoa.metodo('Olá, Mundo!');
+console.log(metodo);
