46cdacc3cc4e6634213472fd4518b1a4ea00db35
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2026-01-18 20:03:31 -0300

    A0062-bibliotecas-externas

---
+++ src/A0062-bibliotecas-externas/A0062-bibliotecas-externas.ts
@@ -0,0 +1,13 @@
+/*
+
+npm i --biblioteca--
+npm i @types/--biblioteca-- -D
+
+Ex.: npm i validator
+npm i @types/validator -D
+
+*/
+
+import validator from 'validator';
+
+console.log(validator.isEmail('vinicius@gmail.com'));
